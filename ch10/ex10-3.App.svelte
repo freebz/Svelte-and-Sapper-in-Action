dee1a0127feb9275b8e63d20391d5c3a7125f04a
freebz <freebz.dev@gmail.com>
2021-09-20 00:18:51 +0900

    10 Animation

---
+++ ch10/ex10-3.App.svelte
@@ -0,0 +1,16 @@
+<!-- Listing 10.3 App that uses Pie component -->
+
+<script>
+  import {tweened} from 'svelte/motion';
+  import Pie from './Pie.svelte';
+
+  let percent = 0;
+  const store = tweened(0, {duration: 1000});
+  $: store.set(percent || 0);
+</script>
+
+<label>
+  Percetn
+  <input type="number" min="0" max="100" bind:value={percent}>
+</label>
+<Pie size={200} percent={$store} />
---
+++ ch10/ex10-7.App.svelte
@@ -0,0 +1,26 @@
+<!-- Listing 10.7 App that uses the ButtonList component -->
+
+<script>
+  import ButtonList from './ButtonList.svelte';
+
+  let left = ['red', 'orange', 'yellow', 'green'];
+  let right = ['blue', 'purple'];
+
+  function move(event, from, to) {
+    const text = event.target.textContent.trim();
+    to.push(text);
+    return [from.filter(t => t !== text), to];
+  }
+
+  function moveLeft(event) {
+    [right, left] = move(event, right, left);
+  }
+
+  function moveRight(event) {
+    [left, right] = move(event, left, right);
+  }
+</script>
+
+<p>Click a button to move it to the other list.</p>
+<ButtonList list={left} moveFn={moveRight} />
+<ButtonList list={right} moveFn={moveLeft} />
