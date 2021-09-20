4e0c5a24eb8903e15e8a89b09a287400b51309cd
freebz <freebz.dev@gmail.com>
2021-09-20 20:20:02 +0900

    11 Debugging

---
+++ ch11/ex11-1.App.svelte
@@ -0,0 +1,17 @@
+<!-- Listing 11.1 App that uses @debug -->
+
+<script>
+  export let radius = 1;
+  $: area = 2 * Math.PI * radius**2;
+</script>
+
+{@debug radius, area}
+<main>
+  <label>
+    Circle Radius:
+    <input type="number" bind:value={radius}>
+  </label>
+  <label>
+    Circle Area: {area}
+  </label>
+</main>
