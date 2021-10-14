66f5a0f7460b0ca0761d62425b196dbd87973183
freebz <freebz.dev@gmail.com>
2021-10-14 22:00:05 +0900

    15 Your first Sapper app

---
+++ ch15/Nav.svelte
@@ -0,0 +1,16 @@
+<!-- Nav component in src/components/Nav.svelte -->
+
+<ul>
+  <li>
+    <a aria-current="{segment === undefined ? 'page' : undefined}"
+       href=".">Shop</a>
+  </li>
+  <li>
+    <a aria-current="{segment === 'cart' ? 'page' : undefined}"
+       href="cart">Cart</a>
+  </li>
+  <li>
+    <a aria-current="{segment === 'ship' ? 'page' : undefined}"
+       href="ship">Ship</a>
+  </li>
+</ul>
---
+++ ch15/sapper-app/src/routes/cart.svelte
@@ -0,0 +1,42 @@
+<svelte:head>
+  <title>Cart</title>
+</svelte:head>
+
+<script>
+  import {cartStore} from '../stores';
+
+  let total =
+      $cartStore.reduce((acc, item) => acc + item.price * item.quantity, 0);
+</script>
+
+<h1>Cart</h1>
+
+{#if $cartStore.length === 0}
+  <div>empty</div>
+{:else}
+  <table>
+    <thead>
+      <tr>
+	<th>Description</th>
+	<th>Quantity</th>
+	<th>Price</th>
+      </tr>
+    </thead>
+    <tbody>
+      {#each $cartStore as item}
+	<tr>
+	  <td>{item.description}</td>
+	  <td>{item.quantity}</td>
+	  <td>${item.price.toFixed(2)}</td>
+	</tr>
+      {/each}
+    </tbody>
+    <tfoot>
+      <tr>
+	<td colspan="2"><label>Total</label></td>
+	<td></td>
+	<td>${total.toFixed(2)}</td>
+      </tr>
+    </tfoot>
+  </table>
+{/if}
