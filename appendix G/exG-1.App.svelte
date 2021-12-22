4cb070d2d7c086e4f2e5a111bd80f8801e79389b
freebz <freebz.dev@gmail.com>
2021-12-23 03:55:34 +0900

    appendix G. Snowpack

---
+++ appendix G/exG-1.App.svelte
@@ -0,0 +1,24 @@
+<!-- Listing G.1 App that uses the DateDisplay component in src/Arr.svelte -->
+
+<script>
+  import _ from 'lodash';
+  import DateDisplay from './DateDisplay';
+
+  let name = 'Snowpack';
+</script>
+
+<h1>Hello, {_.startCase(name)}!</h1>
+
+<label>
+  Name
+  <input bind:value={name}>
+</label>
+
+<DateDisplay />
+
+<style>
+  h1 {
+    color: red;
+  }
+</style>
+
---
+++ appendix G/exG-2.DateDisplay.svelte
@@ -0,0 +1,10 @@
+<!-- Listing G.2 DateDisplay component in src/DateDisplay.svelte -->
+
+<script>
+  import {format} from 'date-fns';
+</script>
+
+<div>
+  Today is {format(new Date(), 'MMM dd, yyyy')}.
+</div>
+
